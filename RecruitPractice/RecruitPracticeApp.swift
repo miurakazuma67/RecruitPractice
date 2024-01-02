3e10f2125437b9fc732ac2c096d05a13eb4220d4
miurakazuma67 <miurakazuma67@gmail.com>
2024-01-02 17:29:18 +0900

    Initial Commit

---
+++ RecruitPractice/RecruitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecruitPracticeApp.swift
+//  RecruitPractice
+//
+//  Created by 三浦　一真 on 2024/01/02.
+//
+
+import SwiftUI
+
+@main
+struct RecruitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
